8b0e95e79079782ee3b945847cb20838e23988df
David N <paperish@icloud.com>
2025-11-05 09:03:29 -0700

    Final event handling lab

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,8 +1,14 @@
 // Code PasswordInput Component Here
 
+function handleChange() {
+    console.log('Entering password...');
+};
+
 function PasswordInput (){
     return(
-        <></>
+        <>
+        <input type="password" onChange={handleChange}/>
+        </>
     )
 }
 
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,8 +1,18 @@
 // Code SubmitButton Component Here
 
+function handleEnter() {
+    console.log('Mouse Entering');
+}
+
+function handleLeave() {
+    console.log('Mouse Exiting');
+}
+
 function SubmitButton (){
     return(
-        <></>
+        <>
+        <button type="submit" onMouseEnter={handleEnter} onMouseLeave={handleLeave}>Submit Password</button>
+        </>
     )
 }
 
